92ecd4c8ae37d1df47cbe30c708d2e14a912e589
BHyeonKim <46583212+BHyeonKim@users.noreply.github.com>
2021-10-25 19:43:36 +0900

    add Jframe

---
+++ src/db/CardBack.java
@@ -0,0 +1,39 @@
+package db;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class CardBack extends JPanel implements MouseListener {
+    public CardBack(){
+        setVisible(true);
+        setBounds(100,100, 400, 400);
+        setBackground(Color.black);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
---
+++ src/db/FlagPane.java
@@ -0,0 +1,40 @@
+package db;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class FlagPane extends JPanel implements MouseListener {
+
+    public FlagPane() {
+        setVisible(true);
+        setBounds(100,100, 400, 400);
+        setBackground(Color.black);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
